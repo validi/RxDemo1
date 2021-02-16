d6bdc4d2ef4ab0839196dc13e912a7ad7bf333e4
monem validi <a.validi6016@gmail.com>
2021-02-16 20:16:37 +0330

    Efficient Way Of Coding

--- app/src/main/java/com/aurora/rxdemo1/MainActivity.java
+++ app/src/main/java/com/aurora/rxdemo1/MainActivity.java
@@ -16,26 +16,26 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String TAG="MyTag";
-    private String greeting="Hello From RxJava";
+    public static String TAG = "MyTag";
+    private String greeting = "Hello From RxJava";
     private Observable<String> myObservable;
-   // private Observer<String>myObserver;
+    // private Observer<String>myObserver;
     private DisposableObserver<String> myDisposableObserver;
     TextView txtGreeting;
-   // private Disposable disposable;
-    CompositeDisposable compositeDisposable=new CompositeDisposable();
+    // private Disposable disposable;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtGreeting= (TextView) findViewById(R.id.txtGreeting);
+        txtGreeting = (TextView) findViewById(R.id.txtGreeting);
 
-        myObservable=Observable.just(greeting);
+        myObservable = Observable.just(greeting);
 
-        myObservable.subscribeOn(Schedulers.io());
-        myObservable.observeOn(AndroidSchedulers.mainThread());
+        //  myObservable.subscribeOn(Schedulers.io());
+        //  myObservable.observeOn(AndroidSchedulers.mainThread());
 
 //        myObserver=new Observer<String>() {
 //            @Override
@@ -63,32 +63,38 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        };
 
-        myDisposableObserver =new DisposableObserver<String>() {
+        myDisposableObserver = new DisposableObserver<String>() {
             @Override
             public void onNext(@NonNull String s) {
-                Log.i(TAG,"onNext");
+                Log.i(TAG, "onNext");
                 txtGreeting.setText(s);
             }
 
             @Override
             public void onError(@NonNull Throwable e) {
-                Log.i(TAG,"onError");
+                Log.i(TAG, "onError");
             }
 
             @Override
             public void onComplete() {
-                Log.i(TAG,"onComplete");
+                Log.i(TAG, "onComplete");
 
             }
         };
-        compositeDisposable.add(myDisposableObserver);
-        myObservable.subscribe(myDisposableObserver);
+        // compositeDisposable.add(myDisposableObserver);
+        //myObservable.subscribe(myDisposableObserver);
+
+        compositeDisposable.add(
+                myObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(myDisposableObserver));
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-       // myDisposableObserver.dispose();
+        // myDisposableObserver.dispose();
         compositeDisposable.clear();
     }
 }
